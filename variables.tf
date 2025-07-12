bd5b66f6116a39a604ca47764d622018a55c93df
durgababu <durgababu.nallagachu@gmail.com>
2025-07-12 13:57:25 +0530

    added required files

--- variables.tf
+++ variables.tf
@@ -7,11 +7,11 @@ variable "environment" {
 }
 
 variable "zone_id" {
-    default = "Z032558618100M4EJX8X4"
+    default = "Z10031831EHO6D6LW9W2V"
 }
 
 variable "zone_name" {
-    default = "daws84s.site"
+    default = "saijyo.store"
 }
 
 variable "component" {
